0b0569ec38211849848636402d1df7619331c936
Si Hyeong Lee <super2451894@gmail.com>
2021-04-14 16:34:36 +0900

    render 라이프사이클 학습
    - render 라이프사이클 학습

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import LifecycleEx from './R004_LifecycleEx'
 
 function App() {
   return (
     <div>
       <h1>리액트 200제 스터디 시작</h1>
       <p>HTML 적용하기 p태그 기존의 HTML 코드들을 사용하는게 가능한듯하다.</p>
+      <LifecycleEx></LifecycleEx>
     </div>
   );
 }
